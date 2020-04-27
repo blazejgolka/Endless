04259706280cc2499b059c51fe3f2d04676621fc
blazejgolka <blazej.golka@gmail.com>
2020-04-27 21:53:37 +0200

    tutorial dalej

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    [SerializeField]
+    [Range(1, 10)]
+    private float jumpPower;
+    public float JumpPower => jumpPower;
+    
+    private Rigidbody2D mojRigidbody;
+    private float mnoznikSkoku = 20f;
+    private bool isGrounded = false;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mojRigidbody = this.transform.GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D inny)
+    {
+        if (inny.collider.tag.Equals("Ground"))
+        {
+            isGrounded = true;
+        }
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D inny)
+    {
+        if (inny.collider.tag.Equals("Ground"))
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D inny)
+    {
+        if (inny.collider.tag.Equals("Ground"))
+        {
+            isGrounded = false;
+        }
+
+    }
+
+
+    // Detects if the shift key was pressed
+    void OnGUI()
+    {
+        GUILayout.Label("Press Enter To Start Game");
+
+        if (Event.current.Equals(Event.KeyboardEvent("[enter]")))
+        {
+            //Application.LoadLevel(1);
+            Debug.LogFormat("przycisk naciśnięty <color=yellow>{0}</color>", "ENTER" );
+        }
+
+        if (Event.current.Equals(Event.KeyboardEvent("return")))
+        {
+            Debug.LogFormat("przycisk naciśnięty <color=red> {0}</color>", "RETURN");
+            
+        }
+
+        if (Event.current.Equals(Event.KeyboardEvent("space")) && isGrounded)
+        {
+            Debug.LogFormat("przycisk naciśnięty <color=green> {0}</color>", "SPACE");
+            mojRigidbody.AddForce(Vector3.up * (jumpPower * mojRigidbody.mass * mojRigidbody.gravityScale * mnoznikSkoku));
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/PrzeszkodaControl.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrzeszkodaControl : MonoBehaviour
+{
+
+    public float scrollerSpeed = 5.0f;
+    public GameObject[] Przeszkoda;
+    public float frequency = 0.5f; // ilość przeszkód na sekundę
+    private float counter = 0.0f;
+    public Transform przeszkodaSpawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GererateRandoPrzeszkoda();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Generowanie przeszkód
+        if (counter <= 0.0f)
+        {
+            GererateRandoPrzeszkoda();
+        } 
+        else
+        {
+            counter -= Time.deltaTime * frequency;
+
+        }
+
+
+
+        //Scroling
+        GameObject currentChild;
+        for (int a0=0; a0<this.transform.childCount; a0++)
+        {
+            currentChild = this.transform.GetChild(a0).gameObject;
+            ScrolerPrzeszkody(currentChild);
+            if (currentChild.transform.position.x <= -15.0f)
+            {
+                Destroy(currentChild);
+            }
+
+        }
+
+    }
+
+    void ScrolerPrzeszkody(GameObject aktualnaPrzeszkoda)
+    {
+        aktualnaPrzeszkoda.transform.position -= Vector3.right * (scrollerSpeed * Time.deltaTime);
+    }
+
+    void GererateRandoPrzeszkoda ()
+    {
+        GameObject newPrzeszkoda =  Instantiate(Przeszkoda[Random.Range(0, Przeszkoda.Length)], przeszkodaSpawnPoint.position, Quaternion.identity) as GameObject;
+        newPrzeszkoda.transform.parent = this.transform;
+        counter = 1.0f;
+    }
+
+
+}
